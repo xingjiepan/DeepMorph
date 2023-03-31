cb463e826ba37293ada639efbb288ad94d949fd4
Xingjie Pan <xingjiepan@gmail.com>
2023-03-31 13:43:41 -0400

    Implement training with both train and validation sets.

--- deepmorph/vqvae/train_vqvae.py
+++ deepmorph/vqvae/train_vqvae.py
@@ -11,7 +11,7 @@ import deepmorph.data.dataset
 import deepmorph.vqvae.vqvae
 
 
-def transform_img_stack(img_stack):
+def transform_img_stack(img_stack, p_channel_dropout=0):
     
     # Transform the images
     policy = torchvision.transforms.Compose([
@@ -23,13 +23,80 @@ def transform_img_stack(img_stack):
     for i in range(len(img_stack)):
         img_stack[i] = policy(img_stack[i])
         
-    # Randomly dropout channels
-    dropout = torch.nn.Dropout2d(p=0.05, inplace=True)
-    dropout(img_stack)
+    # Randomly dropout channels if required
+    if p_channel_dropout > 0:
+        dropout = torch.nn.Dropout2d(p=p_channel_dropout, inplace=True)
+        dropout(img_stack)
+        
+
+def calc_losses(loader, model, device, n_categories=1, normalization_factor=1):
+    '''Calculate the losses on a dataset.'''
+    model.eval()
+    
+    recon_loss_fn = torch.nn.MSELoss()
+    classify_loss_fn = torch.nn.CrossEntropyLoss()
+
+    latent_loss_weight = 0.25
+    classify_loss_weight = 1
+
+    (n_samples, total_loss_sum, recon_loss_sum, latent_loss_sum,
+     t_classify_loss_sum, b_classify_loss_sum) = 0, 0, 0, 0, 0, 0
+    
+    for i, (img, y) in enumerate(loader):
+        
+        model.zero_grad()
+        
+        # Send the image to GPU and normalize the image
+        img = img.to(device, dtype=torch.float32)
+        img = img / normalization_factor
+        y = y.to(device)
+                
+        out, latent_loss, class_t, class_b = model(img)
+        
+        # Calcualte the losses
+        recon_loss = recon_loss_fn(out, img)
+        latent_loss = latent_loss.mean()
+        
+        if n_categories > 1:
+            t_classify_loss = classify_loss_fn(class_t, y)
+            b_classify_loss = classify_loss_fn(class_b, y)
+        else:
+            t_classify_loss = torch.tensor(0)
+            b_classify_loss = torch.tensor(0)
+        
+        loss = recon_loss + latent_loss_weight * latent_loss \
+               + classify_loss_weight * (t_classify_loss + b_classify_loss)
+                
+        # Record the loss history
+        comm = {
+            'n_samples': img.shape[0],
+            'total_loss_sum' : loss.item() * img.shape[0],
+            'recon_loss_sum' : recon_loss.item() * img.shape[0],
+            'latent_loss_sum' : latent_loss.item() * img.shape[0],
+            't_classify_loss_sum': t_classify_loss.item() * img.shape[0],
+            'b_classify_loss_sum': b_classify_loss.item() * img.shape[0],
+        }
+        comm = deepmorph.distributed.all_gather(comm)
+                
+        for part in comm:
+            n_samples += part["n_samples"]
+            total_loss_sum += part["total_loss_sum"]
+            recon_loss_sum += part["recon_loss_sum"]
+            latent_loss_sum += part["latent_loss_sum"]
+            t_classify_loss_sum += part["t_classify_loss_sum"]
+            b_classify_loss_sum += part["b_classify_loss_sum"]
+        
+       
+    model.train()
+    return (n_samples, total_loss_sum, recon_loss_sum, latent_loss_sum,
+           t_classify_loss_sum, b_classify_loss_sum)
+    
     
 
 def train(epoch, loader, model, optimizer, scheduler, device,
           n_categories=1, normalization_factor=1):
+    '''Train one epoch.'''
+    
     if deepmorph.distributed.is_primary():
         loader = tqdm(loader)
     
@@ -105,13 +172,17 @@ def build_model_and_train(args):
     device = args.get('device', 'cuda')
     args['distributed'] = deepmorph.distributed.get_world_size() > 1
     
-    # Create the dataset
-    dataset = deepmorph.data.dataset.DiskDataset(args['data_path'])
-    sampler = deepmorph.distributed.data_sampler(dataset, shuffle=True, distributed=args['distributed'])
-    loader = torch.utils.data.DataLoader(dataset, args['batch_size'], sampler=sampler, num_workers=0)
+    # Create the datasets
+    train_dataset = deepmorph.data.dataset.DiskDataset(args['train_data_path'])
+    train_sampler = deepmorph.distributed.data_sampler(train_dataset, shuffle=True, distributed=args['distributed'])
+    train_loader = torch.utils.data.DataLoader(train_dataset, args['batch_size'], sampler=train_sampler, num_workers=0)
+    
+    val_dataset = deepmorph.data.dataset.DiskDataset(args['validation_data_path'])
+    val_sampler = deepmorph.distributed.data_sampler(val_dataset, shuffle=True, distributed=args['distributed'])
+    val_loader = torch.utils.data.DataLoader(val_dataset, args['batch_size'], sampler=val_sampler, num_workers=0)
     
     # Build the model
-    model = deepmorph.vqvae.vqvae.VQVAE(in_channel=dataset[0][0].shape[0],
+    model = deepmorph.vqvae.vqvae.VQVAE(in_channel=train_dataset[0][0].shape[0],
                        n_categories=args['n_categories'], img_xy_shape=args['img_xy_shape']).to(device)
     
     if args['distributed']:
@@ -134,30 +205,48 @@ def build_model_and_train(args):
     if deepmorph.distributed.is_primary():
         os.makedirs(os.path.join(args['output_path'], 'check_points'), exist_ok=True)
     
-    loss_history = {'total_loss': [], 'recon_loss' : [], 'latent_loss' : [],
-                    't_classify_loss': [], 'b_classify_loss': []}
+    loss_history = {'train_total_loss': [], 'train_recon_loss' : [], 'train_latent_loss' : [],
+                    'train_t_classify_loss': [], 'train_b_classify_loss': [],
+                    'val_total_loss': [], 'val_recon_loss' : [], 'val_latent_loss' : [],
+                    'val_t_classify_loss': [], 'val_b_classify_loss': [],
+                   }
                     
     best_loss = np.float('inf')
-        
     for i in range(args['epoch']):
-        (n_samples, total_loss_sum, recon_loss_sum, latent_loss_sum, 
-         t_classify_loss_sum, b_classify_loss_sum) = train(i, loader, model, optimizer, scheduler, device,
+        
+        # Train one epoch
+        (train_n_samples, train_total_loss_sum, train_recon_loss_sum, train_latent_loss_sum, 
+         train_t_classify_loss_sum, train_b_classify_loss_sum) = train(
+                            i, train_loader, model, optimizer, scheduler, device,
                             n_categories=args['n_categories'], normalization_factor=args['normalization_factor'])
         
-        if n_samples > 0:
-            loss_history['total_loss'].append(total_loss_sum / n_samples)
-            loss_history['recon_loss'].append(recon_loss_sum / n_samples)
-            loss_history['latent_loss'].append(latent_loss_sum / n_samples)
-            loss_history['t_classify_loss'].append(t_classify_loss_sum / n_samples)
-            loss_history['b_classify_loss'].append(b_classify_loss_sum / n_samples)
+        # Calculate losses on the validation dataset
+        (val_n_samples, val_total_loss_sum, val_recon_loss_sum, val_latent_loss_sum, 
+         val_t_classify_loss_sum, val_b_classify_loss_sum) = calc_losses(val_loader, model, device, 
+                    n_categories=args['n_categories'], normalization_factor=args['normalization_factor'])
+        
+        # Record the train history
+        if train_n_samples > 0:
+            loss_history['train_total_loss'].append(train_total_loss_sum / train_n_samples)
+            loss_history['train_recon_loss'].append(train_recon_loss_sum / train_n_samples)
+            loss_history['train_latent_loss'].append(train_latent_loss_sum / train_n_samples)
+            loss_history['train_t_classify_loss'].append(train_t_classify_loss_sum / train_n_samples)
+            loss_history['train_b_classify_loss'].append(train_b_classify_loss_sum / train_n_samples)
+            
+            loss_history['val_total_loss'].append(val_total_loss_sum / val_n_samples)
+            loss_history['val_recon_loss'].append(val_recon_loss_sum / val_n_samples)
+            loss_history['val_latent_loss'].append(val_latent_loss_sum / val_n_samples)
+            loss_history['val_t_classify_loss'].append(val_t_classify_loss_sum / val_n_samples)
+            loss_history['val_b_classify_loss'].append(val_b_classify_loss_sum / val_n_samples)
+        
         
         if deepmorph.distributed.is_primary():
             # Save the model
             torch.save(model.state_dict(), os.path.join(args['output_path'], 'check_points',
                                                        'vqvae_latest.pt'))
             
-            if best_loss > loss_history['total_loss'][-1]:
-                best_loss = loss_history['total_loss'][-1]
+            if best_loss > loss_history['val_total_loss'][-1]:
+                best_loss = loss_history['val_total_loss'][-1]
                 torch.save(model.state_dict(), os.path.join(args['output_path'], 'check_points',
                                                        'vqvae_best.pt'))
             
@@ -169,7 +258,12 @@ def build_model_and_train(args):
             # Save the training history
             with open(os.path.join(args['output_path'], 'loss_history.json'), 'w') as f:
                 json.dump(loss_history, f)
-    
 
+        
+        # Stop the training if the validation loss do not decrease for a number of epochs
+        if 'n_stop_threhold' in args:
+            best_loss = np.min(loss_history['val_total_loss'])
+            if best_loss < np.min(loss_history['val_total_loss'][-args['n_stop_threhold']:]):
+                break
 
     
